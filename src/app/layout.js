982df585c21ec2e870a1a728dd5cd6dcbcfa5466
RA Sumon <rusulazomsumon@gmail.com>
2026-07-25 20:59:01 +0600

    Added: Bing Meta Data for webmaster tools

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ const hindSiliguri = Hind_Siliguri({
 export const metadata = {
   title: "BCS Spark - বাংলাদেশের প্রথম AI-চালিত BCS প্রস্তুতি প্ল্যাটফর্ম",
   description: "BCS Spark হল বাংলাদেশের প্রথম AI-চালিত BCS প্রস্তুতি প্ল্যাটফর্ম। ৪৬তম BCS প্রিলিমিনারি পরীক্ষা সহ সব ধরণের সরকারি চাকরির প্রস্তুতি নিন সহজেই।",
+  // bing webmaster verifications
+  verification: {
+    other: {
+      "msvalidate.01": "6210F62E302522A9230705CDAA6F6019",
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
